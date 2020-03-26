be82719624c988f853ac66098aa7f820370ea1cf
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-03-26 14:42:33 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,14 @@
 def unsafe?(speed)
-if speed == >"60" || speed == <"40" 
-  puts "unsafe"
+if speed > 60 || speed <40 
+  "true"
 else 
-  puts "safe"
+  "false"
+end
 end
 
 
-
 def not_safe?(speed)
-	
+	speed <40 || speed > 60 ? "true" : "false"
 end
 	
 
